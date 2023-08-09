d77a137269bc60691859c37d6d5bc9633d142a11
JanetRW <jane_row@hotmail.com>
2023-08-10 00:53:31 +0200

    arreglando reto2js

--- Retoslibrerias/reto2.js
+++ Retoslibrerias/reto2.js
@@ -1,40 +1,43 @@
-$(document).ready(function() {
-    let miCarrito = [];
+let prenda1  = {type :"Pantalón suelto gris", price:20};
+let prenda2  = {type :"Vestido Tunico", price:18};
+let prenda3  = {type :"Falda Mid Print", price:12};
+let prenda4  = {type :"Cardigan Largo Rayas", price:12};
+let prenda5  = {type :"Short Animal Print", price:12};
+let prenda6  = {type :"Peto de Lino", price:17};
+let prenda7  = {type :"Vestido ajustado", price:10};
+let prenda8  = {type :"Vaquero negro palazzo", price:15};
+let prendas = [];
 
-    // calcula el precio total de los productos en mi carrito
-    function calcularTotal() {
-      let total = 0;
-      miCarrito.forEach(producto => {
-        total += producto.price;
-      });
-      return total;
-    }
+jQuery(() =>{
+    $("#prenda1").on("click",() =>{ prendas.push(prenda1);});
+    $("#prenda2").on("click",() =>{ prendas.push(prenda2);});
+    $("#prenda3").on("click",() =>{ prendas.push(prenda3);});
+    $("#prenda4").on("click",() =>{ prendas.push(prenda4);});
+    $("#prenda5").on("click",() =>{ prendas.push(prenda5);});
+    $("#prenda6").on("click",() =>{ prendas.push(prenda6);});
+    $("#prenda7").on("click",() =>{ prendas.push(prenda7);});
+    $("#prenda8").on("click",() =>{ prendas.push(prenda8);});
 
-    // Función para actualizar mi carrito modal
-    function actualizar() {
-      let listaMiCarrito = $("#listaMiCarrito");
-      let carritoTotal = $("#carritoTotal");
+    $("#micarrito").on("click",()=>{//$("#carritoTotal").empty();
+                                     //$("#suma").empty();
+                                    $("#carritolista").modal("show");
+                                    let totalCarrito = 0;
+        
 
-      listaMiCarrito.empty();
-      miCarrito.forEach(producto => {
-        listaMiCarrito.append(`<li class="list-group-item">PRENDA: ${producto.name} - ${producto.price}€</li>`);
-      });
-
-      carritoTotal.text(calcularTotal() + "€");
-      carritoTotal.css("color", "#d63384");
-    }
-
-    // uso evento para el botón "Añadir al carrito"
-    $(".btn-primary").click(function() {
-      let nombre = $(this).closest(".card-body").find(".card-title").text();
-      let precio = parseFloat($(this).closest(".card-body").find(".card-price").text());
-
-      miCarrito.push({ name: nombre, price: precio });
-      actualizar();
-    });
-
-    // muestro el carrito al hacer clic en el botón "Carrito"
-    $(".btn-secondary").click(function() {
-      $("#carritoModal").modal("show");
-    });
-  });
+                                    
+                                    
+                                    //$("#totalCarrito").empty();
+                                    //$("#suma").empty();
+                                    for(ropa of prendas){
+                                            
+                                            $("#totalCarrito").append("PRENDA: " +ropa.type + " "+ ropa.price + "€<br>")
+                                            totalCarrito += ropa.price;
+                                        }
+                                            $("#suma").append("<p> TOTAL COMPRA: " +totalCarrito+"€</p>");
+                                        });
+                                            // Agregar una función para limpiar el carrito cuando se cierre el modal
+    // $("#carritolista").on("hidden.bs.modal", () => {
+    //     $("#carritoTotal").empty();
+    //     $("#suma").empty();
+    //                                     })
+});
